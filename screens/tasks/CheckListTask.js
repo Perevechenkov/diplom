aa66f41daaa059f461edcd376251759068941554
DesperateHiking <perevechenkov_1999@mail.ru>
2021-06-09 22:46:42 +0500

    key in scroll fix

--- screens/tasks/CheckListTask.js
+++ screens/tasks/CheckListTask.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 import { StyleSheet, FlatList, View, Text, Button } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { CONTENT } from '../../data/dummy-data';
 
 export const CheckListTask = props => {
+     const content = CONTENT.filter(
+          contentItem => props.task.id === contentItem.task
+     );
+
      return (
           <View style={styles.screen}>
                <ScrollView>
-                    {props.task.content.map(item => (
-                         <Text key={Date.now()}>{item}</Text>
+                    {content.map(item => (
+                         <Text key={item.id}>{item.body}</Text>
                     ))}
                </ScrollView>
           </View>
